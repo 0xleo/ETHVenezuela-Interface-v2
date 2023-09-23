861eb15abb5d5aac993c8a02ae17c88635920d1e
0xleo <leojim2002@gmail.com>
2023-09-22 22:58:57 -0400

    SVG component update

--- src/components/InfoEND.tsx
+++ src/components/InfoEND.tsx
@@ -1,10 +1,13 @@
 import './InfoEND.css'
+import { ItemsIMG } from './ItemsIMG'
 // import instagram from './src/components/assets/instagram.svg'
 // import twitterX from './src/components/assets/twitterX.svg'
 // import telegram from './src/components/assets/telegram.svg' 
-import { ReactComponent as Instagram} from "./src/components/assets/instagram.svg"
-import { ReactComponent as TwitterX} from "./src/components/assets/twitterX.svg"
-import { ReactComponent as Telegram} from "./src/components/assets/telegram.svg"
+
+// import { ReactComponent as Instagram} from "./src/components/assets/instagram.svg"
+// import { ReactComponent as TwitterX} from "./src/components/assets/twitterX.svg"
+// import { ReactComponent as Telegram} from "./src/components/assets/telegram.svg"
+// import { ItemsIMG } from "./src/components/ItemsIMG.tsx";
 
 export const InfoEND = () => {
   return (
@@ -24,15 +27,15 @@ export const InfoEND = () => {
 
       <section className='section2'>
       <a href="" target="_blank" rel="noreferrer">
-          <img src={Instagram} className="logos" alt="Instagram logo" />
+          <ItemsIMG src={'./src/components/assets/instagram.svg'} className="logos" alt="Instagram logo" />
       </a>
 
       <a href="" target="_blank" rel="noreferrer">
-          <img src={TwitterX} className="logos" alt="TwitterX logo" />
+          <ItemsIMG src={'./src/components/assets/twitterX.svg'} className="logos" alt="TwitterX logo" />
       </a>  
 
       <a href="" target="_blank" rel="noreferrer">
-          <img src={Telegram} className="logos" alt="Telegram logo" />
+          <ItemsIMG src={'./src/components/assets/telegram.svg'} className="logos" alt="Telegram logo" />
       </a>
       </section>
 
